4d22ca28f6314b2da579099fb6e26cc0c5ea5a76
rutikwaghe123 <rutikwaghe123@gmail.com>
2024-03-03 00:40:23 +0530

    comment code

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ app.use(helmet());
 
 //database connection
 mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URL
+  //   , {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // }
+  )
   .then(() => {
     console.log("Mongodb database connected");
   })
